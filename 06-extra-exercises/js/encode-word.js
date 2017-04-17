2d4559a8535eafb36e7bc7c232c74d141de29610
Mario Terron <marioterron157@gmail.com>
2017-04-17 20:28:26 +0200

    added new ways to solve the exercise

--- 06-extra-exercises/js/encode-word.js
+++ 06-extra-exercises/js/encode-word.js
@@ -8,13 +8,62 @@
 // 5 instead of S
 // 0 instead of O
 
-function encodeWord (string) {
-  var lettersInString = string.toLowerCase().split('')
-  for (var letter in lettersInString) {
-    if (lettersInString[letter] === 't') lettersInString[letter] = 7
-    if (lettersInString[letter] === 'a') lettersInString[letter] = 4
-    if (lettersInString[letter] === 's') lettersInString[letter] = 5
-    if (lettersInString[letter] === 'o') lettersInString[letter] = 0
+// Way 1
+function encodeWord (text) {
+  var aText = txt.toLowerCase().split('')
+  for (var letter in aText) {
+    if (aText[letter] === 't') aText[letter] = 7
+    if (aText[letter] === 'a') aText[letter] = 4
+    if (aText[letter] === 's') aText[letter] = 5
+    if (aText[letter] === 'o') aText[letter] = 0
   }
-  return lettersInString.join('')
+  return aText.join('')
 }
+
+// Way 1
+function encodeWord (text) {
+  var myText = text
+  myText = myText.replace(/t/gi, '7')
+  myText = myText.replace(/a/gi, '4')
+  myText = myText.replace(/s/gi, '5')
+  myText = myText.replace(/o/gi, '0')
+  return myText
+}
+
+// Way 2
+function encodeWord (text) {
+  return text
+          .replace(/t/gi, '7')
+          .replace(/a/gi, '4')
+          .replace(/s/gi, '5')
+          .replace(/o/gi, '0')
+}
+
+// Way 3
+function encodeWord (text) {
+  var codes = {
+    't': 7,
+    'a': 4,
+    's': 5,
+    'o': 0
+  }
+  return text.replace(/[taso]/gi, function (itemFound) {
+    return codes[itemFound]
+  })
+}
+
+// Way 4
+function encodeWord (text) {
+  return text.replace(/\w/gi, function (itemFound) {
+    var codes = { 't': 7, 'a': 4, 's': 5, 'o': 0, 'j': 1 }
+    return codes[itemFound] ? codes[itemFound] : itemFound
+  })
+}
+
+// Way 5
+function encodeWord (codes, text) {
+  return text.replace(/\w/gi, function (itemFound) {
+    return codes[itemFound] ? codes[itemFound] : itemFound
+  })
+}
+// encodeWord( { "t": 75, "a": 43, "s": 55, "o": 30, "j": 12 }, "juanma")
